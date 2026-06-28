c443db6e68dd14f9893b00619ad622d6b15e14b2
PavelMizev <pma72amp@gmail.com>
2026-06-28 20:35:09 +0500

    En: Editing for developer convenience. Ru: Редактирование для удобства разработчиков.

--- src/main/kotlin/modules/Calculator.kt
+++ src/main/kotlin/modules/Calculator.kt
@@ -3,6 +3,7 @@ package com.turashift.kotlincmd.modules
 import com.calib.CaLib
 
 private fun calc_work() {
+
     println("Enter a mathematical expression for example: \"2 + 2 * 2\" or \"(10 / 2) ^ 3\".")
     print("Expression: ")
     val input = readln()
@@ -13,17 +14,23 @@ private fun calc_work() {
     }
 
     try {
+
         val result = CaLib.calculate(input)
         println("Answer: $result")
+
     } catch (e: IllegalArgumentException) {
+
         println("Calculation error: ${e.message}")
+
     }
+
 }
 
 fun calc() {
     var input: String
 
     while (true) {
+
         calc_work()
         print("Continue? [Y/n] ")
         input = readln()
@@ -32,10 +39,15 @@ fun calc() {
             "Y", "y" -> {}
             "N", "n" -> break
             else -> {
+
                 println("Exit cancelled due to user input error.")
+
             }
+
         }
+
     }
+
 }
 
 const val CALC_COMMAND = "calc"
